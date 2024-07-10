b9c392335654ec5ef8be524b969f76e7410abf77
MUSTKEEM AHMAD <46074466+Mustkeem324@users.noreply.github.com>
2024-07-10 20:51:21 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://shaktibtcs38:shakti1234@cluster0.4whuzao.mongodb.net/?retryWrites=true&w=majority';
+// mongoose.connect('url') 
+mongoose.connect(url);
+const db=mongoose.connection;
+
+//If any Error then Getting this Line
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+
+
+db.once('open',()=>{
+    console.log("Connected to Database :: MongoDB ")
+});
+
+module.exports=db;  //Exports db
